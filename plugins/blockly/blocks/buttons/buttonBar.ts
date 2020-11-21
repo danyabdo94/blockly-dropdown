54b4878dadf47602e96a6c22ebeaf30d1fe4d1f8
danyabdo <danyabdo94@gmail.com>
2020-11-21 20:37:28 +0200

    Add the buttons inputs

--- plugins/blockly/blocks/buttons/buttonBar.ts
+++ plugins/blockly/blocks/buttons/buttonBar.ts
@@ -26,4 +26,75 @@ Blockly.Blocks[blockName] = {
     init () {
         this.jsonInit(blockDefinition);
     },
+    data: JSON.stringify(
+        [
+            {
+                name: 'layout',
+                _type: 'accordion',
+                children: [
+                    {
+                        name: 'theme',
+                        _type: 'select',
+                        options: [
+                            'spaceAround',
+                            'spaceBetween',
+                            'spaceEvenly',
+                            'center',
+                            'start',
+                            'end',
+                        ],
+                    },
+                    {
+                        name: 'overflowSpacing',
+                        _type: 'number',
+                    },
+                ],
+            },
+            {
+                name: 'buttons',
+                _type: 'accordion',
+                children: [
+                    {
+                        name: 'theme',
+                        _type: 'select',
+                        options: [
+                            'accent',
+                            'normal',
+                            'primary',
+                        ],
+                    },
+                    {
+                        name: 'minWidth',
+                        _type: 'number',
+                    },
+                    {
+                        name: 'overflowSpacing',
+                        _type: 'minHeight',
+                    },
+                    {
+                        name: 'padding',
+                        _type: 'accordion',
+                        children: [
+                            {
+                                name: 'top',
+                                _type: 'number',
+                            },
+                            {
+                                name: 'bottom',
+                                _type: 'number',
+                            },
+                            {
+                                name: 'left',
+                                _type: 'number',
+                            },
+                            {
+                                name: 'right',
+                                _type: 'number',
+                            },
+                        ],
+                    },
+                ],
+            },
+        ],
+    ),
 } as Block;
--- plugins/blockly/blocks/buttons/dropdownButton.ts
+++ plugins/blockly/blocks/buttons/dropdownButton.ts
@@ -21,4 +21,48 @@ Blockly.Blocks[blockName] = {
     init () {
         this.jsonInit(blockDefinition);
     },
+    data: JSON.stringify(
+        [
+            {
+                name: 'button',
+                _type: 'accordion',
+                children: [
+                    {
+                        name: 'underline',
+                        _type: 'number',
+                    },
+                    {
+                        name: 'hint',
+                        _type: 'text',
+                    },
+                    {
+                        name: 'disabledHint',
+                        _type: 'text',
+                    },
+                ],
+            },
+            {
+                name: 'menu',
+                _type: 'accordion',
+                children: [
+                    {
+                        name: 'elevation',
+                        _type: 'number',
+                    },
+                    {
+                        name: 'focusColor',
+                        _type: 'color',
+                    },
+                    {
+                        name: 'autofocus',
+                        _type: 'boolean',
+                    },
+                    {
+                        name: 'backgroundColor',
+                        _type: 'color',
+                    },
+                ],
+            },
+        ],
+    ),
 } as Block;

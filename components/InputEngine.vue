85779f3f18191e5cda930aff22a5a13e209dbcf8
danyabdo <danyabdo94@gmail.com>
2020-11-21 20:36:51 +0200

    Add checkbox for engine

--- components/InputEngine.vue
+++ components/InputEngine.vue
@@ -7,6 +7,10 @@
             v-if="element._type === 'text' || element._type === 'number'"
             :element="element"
         />
+        <checkbox-field
+            v-if="element._type === 'boolean' "
+            :element="element"
+        />
     </div>
 </template>
 
@@ -16,9 +20,10 @@ import Accordion from './atoms/Accordion.vue';
 import ColorPicker from './atoms/ColorPicker.vue';
 import InputSelect from './atoms/InputSelect.vue';
 import InputField from './atoms/InputField.vue';
+import CheckboxField from './atoms/CheckboxField.vue';
 export default Vue.extend({
     name: 'InputEngine',
-    components: { Accordion, ColorPicker, InputSelect, InputField },
+    components: { Accordion, ColorPicker, InputSelect, InputField, CheckboxField },
     props: ['element'], // TODO: Add a type
     updated () {
         console.log(this.element);
---
+++ components/atoms/CheckboxField.vue
@@ -0,0 +1,14 @@
+<template>
+    <v-checkbox
+        :label="element.name"
+    />
+</template>
+
+<script>
+import Vue from 'vue';
+
+export default Vue.extend({
+    name: 'CheckboxField',
+    props: ['element'],
+});
+</script>

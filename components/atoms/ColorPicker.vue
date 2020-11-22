079151bcd2fddab137abcd89ce2b973cfafa2160
danyabdo <danyabdo94@gmail.com>
2020-11-22 03:03:09 +0200

    Fix color picker UI

--- components/atoms/ColorPicker.vue
+++ components/atoms/ColorPicker.vue
@@ -1,39 +1,49 @@
 <template>
-    <v-row v-if="!element.condition || canView()" justify="center" align="center" class="pa-1">
-        <v-col class="shrink" style="min-width: 100%">
-            <v-text-field
-                v-model="color"
-                hide-details
-                class="ma-0 pa-0"
-                solo
+    <v-row
+        v-if="!element.condition || canView()"
+        justify="space-between"
+        class="pa-1"
+        style="min-width: 100%"
+    >
+        <v-col
+            cols="12"
+            md="4"
+        >
+            <v-sheet
+                color="white"
+                width="50%"
             >
-                <template v-slot:append>
-                    <v-menu
-                        v-model="menu"
-                        top
-                        nudge-bottom="105"
-                        nudge-left="16"
-                        :close-on-content-click="false"
-                    >
-                        <template v-slot:activator="{ on }">
-                            <div :style="swatchStyle" v-on="on" />
-                        </template>
-                        <v-card>
-                            <v-card-text class="pa-0">
-                                <v-color-picker
-                                    v-model="color"
-                                    mode="hexa"
-                                    show-swatches
-                                    hide-canvas
-                                    hide-inputs
-                                    flat
-                                    @update:color="change"
-                                />
-                            </v-card-text>
-                        </v-card>
-                    </v-menu>
+                <p>{{ element.name }}</p>
+            </v-sheet>
+        </v-col>
+        <v-col
+            cols="12"
+            md="4"
+        >
+            <v-menu
+                v-model="menu"
+                top
+                nudge-bottom="105"
+                nudge-left="16"
+                :close-on-content-click="false"
+            >
+                <template v-slot:activator="{ on }">
+                    <div :style="swatchStyle" v-on="on" />
                 </template>
-            </v-text-field>
+                <v-card>
+                    <v-card-text class="pa-0">
+                        <v-color-picker
+                            v-model="color"
+                            mode="hexa"
+                            show-swatches
+                            hide-canvas
+                            hide-inputs
+                            flat
+                            @update:color="change"
+                        />
+                    </v-card-text>
+                </v-card>
+            </v-menu>
         </v-col>
     </v-row>
 </template>
@@ -55,7 +65,7 @@ export default Vue.extend({
                 backgroundColor: color,
                 cursor: 'pointer',
                 height: '30px',
-                width: '60px',
+                width: '50px',
                 border: 'solid 1px #000000',
                 borderRadius: menu ? '50%' : '4px',
                 transition: 'border-radius 200ms ease-in-out',
